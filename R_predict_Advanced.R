c352f7c135321d7a651d8ac21e4ba9c719388e0c
kishorkumar89 <shyam.kishor.kumar89@gmail.com>
2020-02-20 23:47:55 +0530

    Making Appropriate Changes in the file.

--- R_predict_Advanced.R
+++ R_predict_Advanced.R
@@ -1,19 +1,24 @@
 gc()
 #setting the Working directory.
-setwd("E:/GIT_Hub/WallMart")
+setwd("E:/WallMart_TimeSeries-master")
+
+#importing Libraries
+library(dplyr)    #loading the 'dplyr' library into the working directory for data transformation
+library(caret)    # Handling the missing Values.
+library(lattice)
+library(ggplot2)  # Visualization
+
 
 #importing the data set.
-store.DS <- read.csv("all/stores.csv")
-train.DS <- read.csv('all/train.csv')
+store.DS    <- read.csv("all/stores.csv")
+train.DS    <- read.csv('all/train.csv')
 features.DS <- read.csv('all/features.csv')
 
-
-#loading the 'dplyr' library into the working directory for data transformation
-library(dplyr)
 #converting Data Frames into Local data frame.
-train.DF <- tbl_df(train.DS)
-store.Df <- tbl_df(store.DS)
+train.DF    <- tbl_df(train.DS)
+store.Df    <- tbl_df(store.DS)
 features.DF <- tbl_df(features.DS)
+
 # removing the data frames from the working directory.
 rm(train.DS,store.DS,features.DS)
 colnames(store.Df)
@@ -21,30 +26,27 @@ colnames(train.DF)
 
 #appying inner_join from the dplyr to the tables store.Df,train.DF
 #on the column Store.
-joined.DF <- inner_join(store.Df,train.DF,by = ('Store'))
+joined.DF <- inner_join(store.Df,train.DF)
 colnames(joined.DF)
 colnames(features.DF)
 
 #formating the Date column of both the tables what R understands
 #making it compitable for inner join.
-joined.DF$Date <- as.Date(joined.DF$Date, format = "%Y-%m-%d")
-features.DF$Date <- as.Date(features.DF$Date,format = "%d-%b-%Y")
+joined.DF$Date   <- as.Date(joined.DF$Date   ,format = "%Y-%m-%d")
+features.DF$Date <- as.Date(features.DF$Date ,format = "%d-%b-%Y")
 
 #appying inner_join from the dplyr to the tables joined.DF and the features.DF
-joined.DF <- inner_join(joined.DF,features.DF,
-                          by = c('Store','Date','IsHoliday'))
+joined.DF <- inner_join(joined.DF,features.DF)
+joined.DF.BKP <- joined.DF # Creating a backup File
 
 #removing all the unused tables.
 rm(features.DF,store.Df,train.DF)
 ###############################################################################
 
 #counting number of NAs in the data set if there is any.
-sum(is.na(joined.DF))
+sapply(joined.DF, function(x) sum(is.na(x)))
 
-##Handling the missing Values.
-library(caret)
-library(lattice)
-library(ggplot2)
+
 
 # incresing the memory space to R
 memory.limit(size = 1024000)
